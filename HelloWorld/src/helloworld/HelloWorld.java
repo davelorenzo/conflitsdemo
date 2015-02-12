92d60f307c988d6ed7868bd4ab5a63eca241eee3
Dave Lorenzo <dave.lorenzo@hotmail.fr>
2015-02-12 12:34:08 -0500

    Petites corrections

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import mystere.Mystere;
+
+/**
+ *
+ * @author Dave
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {  
+        String nom = "";
+        System.out.println("Hello World!");
+        Mystere mystere;
+        mystere = new Mystere();
+        
+            for (int i=0;i<args.length;i++) {
+                if (i!=0) {
+                    nom = nom+" ";
+                }
+                nom = nom + args[i];
+       }
+         System.out.println("Bonjour "+nom+"!");
+    }
+}
